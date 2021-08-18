192d44ca919303a9d2c6f9bd9c497e5a162bb128
Vituz <79053442+Vituz@users.noreply.github.com>
2021-08-18 12:08:47 +0200

    modify welcome page, and add style link

--- public/js/app.js
+++ public/js/app.js
@@ -37457,8 +37457,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\MAMP\htdocs\php\studing-purpose\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\php\studing-purpose\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP\htdocs\php\laravel\studing-purpose\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\php\laravel\studing-purpose\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
